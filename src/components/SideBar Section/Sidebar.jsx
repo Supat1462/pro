a327c592da0295e82f1dfd78a486abd03ed28831
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-02-03 17:42:27 +0700

    test

---
+++ src/components/SideBar Section/Sidebar.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import './sidebar.css'
+
+import logo from '../../asset/Logo@200x.png'
+
+import { BsLayoutTextWindowReverse } from "react-icons/bs";
+import { BsArrowLeftRight } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdDevices } from "react-icons/md";
+import { FcComboChart } from "react-icons/fc";
+
+
+function Sidebar() {
+  return (
+    <div className='sideBar grid'>
+      <div className="logoDiv flex">
+        <img src={logo} alt="Image Name" />
+        <h2>Pro.</h2>
+      </div>
+
+      <div className="menuDev">
+        <h3 className='divTitle'>
+          QUICK MENU
+        </h3>
+        <div className="menuLists grid">
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BsLayoutTextWindowReverse className='icon' />
+              <span className="smallText">Dash board</span>
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BsArrowLeftRight className='icon' />
+              <span className="smallText">Borrow and Return</span>
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <AiOutlineUser className='icon' />
+              <span className="smallText">Employee</span>
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <MdDevices className='icon' />
+              <span className="smallText">Device</span>
+            </a>
+          </li>
+        </div>
+      </div>
+
+      <div className="settingsDiv">
+        <h3 className='divTitle'>
+          SETIINGS
+        </h3>
+        <div className="menuLists grid">
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <FcComboChart className='icon' />
+              <span className="smallText">Dash board</span>
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <BsArrowLeftRight className='icon' />
+              <span className="smallText">Borrow and Return</span>
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="#" className="menuLink flex">
+              <AiOutlineUser className='icon' />
+              <span className="smallText">Employee</span>
+            </a>
+          </li>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
